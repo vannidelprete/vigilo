3d127f4c3231dc770c1493a1f30f4532d97e5828
Giovanni Del Prete <giovannidelprete95@gmail.com>
2026-07-02 19:10:44 +0200

    feat: MPU6050 I2C read and Serial output

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,8 +1,13 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <MPU6050.h>
 #include "secrets.h"
 
 #define LED_PIN 2
+#define I2C_SDA 21
+#define I2C_SCL 22
+
+MPU6050 imu;
 
 void connectWifi() {
     Serial.printf("Connecting to %s", WIFI_SSID);
@@ -18,10 +23,26 @@ void connectWifi() {
 void setup() {
     Serial.begin(115200);
     pinMode(LED_PIN, OUTPUT);
+
+    Wire.begin(I2C_SDA, I2C_SCL);
+    imu.initialize();
+
+    if (!imu.testConnection()) {
+        Serial.println("MPU6050 connection failed");
+        while (true) {}
+    }
+    Serial.println("MPU6050 connected");
+
     connectWifi();
 }
 
 void loop() {
+    int16_t ax, ay, az, gx, gy, gz;
+    imu.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    Serial.printf("ax=%d ay=%d az=%d | gx=%d gy=%d gz=%d\n",
+                  ax, ay, az, gx, gy, gz);
+
     digitalWrite(LED_PIN, HIGH);
     delay(500);
     digitalWrite(LED_PIN, LOW);
