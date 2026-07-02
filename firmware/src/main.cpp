b14aacfc2478761cb8f0163af7d96b3f61270ab9
Giovanni Del Prete <giovannidelprete95@gmail.com>
2026-07-02 17:46:15 +0200

    feat: blink test and WiFi connection

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,8 +1,29 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include "secrets.h"
+
+#define LED_PIN 2
+
+void connectWifi() {
+    Serial.printf("Connecting to %s", WIFI_SSID);
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.printf("\nConnected - IP: %s\n", WiFi.localIP().toString().c_str());
+}
 
 void setup() {
     Serial.begin(115200);
+    pinMode(LED_PIN, OUTPUT);
+    connectWifi();
 }
 
 void loop() {
+    digitalWrite(LED_PIN, HIGH);
+    delay(500);
+    digitalWrite(LED_PIN, LOW);
+    delay(500);
 }
